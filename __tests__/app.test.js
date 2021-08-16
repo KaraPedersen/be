5805e664528dc8737accad7e0621528e63873a99
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-08-16 16:08:52 -0700

    post favorite character test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,14 +4,59 @@ import request from 'supertest';
 import app from '../lib/app.js';
 
 describe('demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+  let user = {
+    username: 'Azlynn',
+    password: 'password'
+  };
+
+  beforeAll(() => {
+    return setup(pool)
+      .then(() => 
+        request(app).post('/api/auth/signup').send(user))
+      .then(res => user = res.body);
   });
+
   test('GET the character', async () => {
     const { body } = await request(app)
-      .get('/characters/');
+      .get('/characters/all');
 
-    expect(body.info.count).toBe(671);
-    expect(body.results.length).toBe(20);
+    expect(body.length).toBe(20);
+  });
+
+  test('add a character as a favorite POST', async () => {
+    const character = {
+      'id': 1,
+      'name': 'Rick Sanchez',
+      'status': 'Alive',
+      'species': 'Human',
+      'type': '',
+      'gender': 'Male',
+      'origin': {
+        'name': 'Earth (C-137)',
+        'url': 'https://rickandMortyapi.com/api/location/1'
+      },
+      'location': {
+        'name': 'Earth(Replacement Dimension)',
+        'url': 'https://rickandmortyapi.com/api/location/20'
+      },
+      'image': 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      'episode': [],
+      'url': 'https://rickandmortyapi.com/api/character/1',
+      'created': '2017-11-04T18:48:46.250Z'
+    };
+
+    const { body } = await request(app)
+      .post('/characters/user')
+      .send({ character, user });
+
+    expect(body).toEqual({
+      characterId: 1,
+      userId: '1',
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      location: '{"name":"Earth(Replacement Dimension)","url":"https://rickandmortyapi.com/api/location/20"}',
+      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+    });
+
   });
 });
